4643a9b53e69b767b0c89d8850530d5c3ba8ade6
Patrick Joyce <pjaudiomv@gmail.com>
2021-12-05 14:36:23 -0500

    add single tz with existing countries in bmlt

---
+++ singleTzCountriesExisting.php
@@ -0,0 +1,265 @@
+<?php
+
+$singleTimezoneCountries = array(
+    "ALLEMAGNE" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Berlin",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "ARGENTINA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => -10800,
+        "timeZoneId" => "America/Argentina/Salta",
+        "timeZoneName" => "Argentina Standard Time"
+    ),
+    "AUSTRIA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Vienna",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "BELGIA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Brussels",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "BM" => array(
+        "dstOffset" => 0,
+        "rawOffset" => -14400,
+        "timeZoneId" => "Atlantic/Bermuda",
+        "timeZoneName" => "Atlantic Standard Time"
+    ),
+    "BOTSWANA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 7200,
+        "timeZoneId" => "Africa/Gaborone",
+        "timeZoneName" => "Central Africa Time"
+    ),
+    "BS" => array(
+        "dstOffset" => 0,
+        "rawOffset" => -18000,
+        "timeZoneId" => "America/Nassau",
+        "timeZoneName" => "Eastern Standard Time"
+    ),
+    "DANIA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Copenhagen",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+
+    "DEUTSCH" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Berlin",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "DEUTSCHLAND" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Berlin",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "ESWATINI" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 7200,
+        "timeZoneId" => "Africa/Mbabane",
+        "timeZoneName" => "South Africa Standard Time"
+    ),
+    "GERMAN" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Berlin",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "GERMANY" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Berlin",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "GUADELOUPE" => array(
+        "dstOffset" => 0,
+        "rawOffset" => -14400,
+        "timeZoneId" => "America/Guadeloupe",
+        "timeZoneName" => "Atlantic Standard Time"
+    ),
+    "IN" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 19800,
+        "timeZoneId" => "Asia/Calcutta",
+        "timeZoneName" => "India Standard Time"
+    ),
+    "INDIA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 19800,
+        "timeZoneId" => "Asia/Calcutta",
+        "timeZoneName" => "India Standard Time"
+    ),
+    "IRELAND" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 0,
+        "timeZoneId" => "Europe/Dublin",
+        "timeZoneName" => "Greenwich Mean Time"
+    ),
+    "IRELND" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 0,
+        "timeZoneId" => "Europe/Dublin",
+        "timeZoneName" => "Greenwich Mean Time"
+    ),
+    "IRLANDIA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 0,
+        "timeZoneId" => "Europe/Dublin",
+        "timeZoneName" => "Greenwich Mean Time"
+    ),
+    "ITALIA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Rome",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "ITALY" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Rome",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "KENYA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 10800,
+        "timeZoneId" => "Africa/Nairobi",
+        "timeZoneName" => "East Africa Time"
+    ),
+    "MAROC" => array(
+        "dstOffset" => 3600,
+        "rawOffset" => 0,
+        "timeZoneId" => "Africa/Casablanca",
+        "timeZoneName" => "GMT+01:00"
+    ),
+    "MOZAMBIQUE" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 7200,
+        "timeZoneId" => "Africa/Maputo",
+        "timeZoneName" => "Central Africa Time"
+    ),
+    "NAMIBIA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 7200,
+        "timeZoneId" => "Africa/Windhoek",
+        "timeZoneName" => "Central Africa Time"
+    ),
+    "NIEMCY" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Berlin",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "NIGERIA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Africa/Lagos",
+        "timeZoneName" => "West Africa Standard Time"
+    ),
+    "NORTHERN IRELAND" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 0,
+        "timeZoneId" => "Europe/London",
+        "timeZoneName" => "Greenwich Mean Time"
+    ),
+    "NORWEGIA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Oslo",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "POLAND" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Warsaw",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "POLSKA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Warsaw",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "SCOTLAND" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 0,
+        "timeZoneId" => "Europe/London",
+        "timeZoneName" => "Greenwich Mean Time"
+    ),
+    "SUISSE" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Zurich",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "SVERGIE" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Stockholm",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "SVERIGE" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Stockholm",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "TANZANIA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 10800,
+        "timeZoneId" => "Africa/Dar_es_Salaam",
+        "timeZoneName" => "East Africa Time"
+    ),
+    "THAILANDE" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 25200,
+        "timeZoneId" => "Asia/Bangkok",
+        "timeZoneName" => "Indochina Time"
+    ),
+    "TT" => array(
+        "dstOffset" => 0,
+        "rawOffset" => -14400,
+        "timeZoneId" => "America/Port_of_Spain",
+        "timeZoneName" => "Atlantic Standard Time"
+    ),
+    "UGANDA" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 10800,
+        "timeZoneId" => "Africa/Kampala",
+        "timeZoneName" => "East Africa Time"
+    ),
+    "URUGUAI" => array(
+        "dstOffset" => 0,
+        "rawOffset" => -10800,
+        "timeZoneId" => "America/Montevideo",
+        "timeZoneName" => "Uruguay Standard Time"
+    ),
+    "ZIMBABWE" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 7200,
+        "timeZoneId" => "Africa/Harare",
+        "timeZoneName" => "Central Africa Time"
+    ),
+    "ÖSTEREICH" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Vienna",
+        "timeZoneName" => "Central European Standard Time"
+    ),
+    "ÖSTERREICH" => array(
+        "dstOffset" => 0,
+        "rawOffset" => 3600,
+        "timeZoneId" => "Europe/Vienna",
+        "timeZoneName" => "Central European Standard Time"
+    )
+);
+
+echo json_encode($singleTimezoneCountries);
